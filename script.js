6773671d1e8230d49268638b86188fe036d6a0e2
AizazJan <157684223+AizazJan@users.noreply.github.com>
2024-12-04 20:19:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav a');
+
+window.addEventListener('scroll', () => {
+    let currentSection = '';
+    
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        if (pageYOffset >= sectionTop - 60) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(currentSection)) {
+            link.classList.add('active');
+        }
+    });
+});
+
+const footerYear = document.querySelector('footer p');
+const currentYear = new Date().getFullYear();
+footerYear.textContent = `© ${currentYear} M Aizaz Tahir. All rights reserved.`;
